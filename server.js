e3914a3caff462e4326a93e90be13a90ea19a893
eb46 <ericjbondoc@gmail.com>
2020-09-22 13:18:39 -0700

    moved all server files to root level, added scripts to package.json server file, and added process.env.NODE_ENV statement to server.js file

---
+++ server.js
@@ -0,0 +1,52 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const db = mongoose.connection
+const path = require('path')
+
+require('dotenv').config()
+
+const app = express()
+const PORT = process.env.PORT || 8000
+
+
+// Middleware
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+
+// Database connection, uses the MongoDB URI from the .env file
+const MONGODB_URI = process.env.MONGODB_URI
+
+
+// Connect to Mongo &
+// Fix Depreciation Warnings from Mongoose
+// May or may not need these depending on your Mongoose version
+mongoose.connect(MONGODB_URI , { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true }
+);
+
+
+// Error / success for MongoDB connection
+db.on('error', (err) => console.log(err.message + ' is Mongod not running?'));
+db.on('connected', () => console.log('mongo connected: ', MONGODB_URI));
+db.on('disconnected', () => console.log('mongo disconnected'));
+
+
+// Controllers
+const applications = require('./controllers/applications_controller')
+app.use('/applications', applications)
+
+if (process.env.NODE_ENV === 'production') {
+  // Exprees will serve up production assets
+  app.use(express.static('client/build'));
+  // Express serve up index.html file if it doesn't recognize route
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+
+app.listen(PORT, () => {
+  console.log('listening on port: ' + PORT);
+})
